d28f466ac2643f6533c3a5b8aa6b971ea85a4182
Norbert Annus <norberta@Norberts-MacBook-Pro.local>
2017-09-17 13:31:25 +0100

    Implement Draw Line

---
+++ src/bit-manipulation/draw-line.spec.ts
@@ -0,0 +1,99 @@
+import { drawLine } from './draw-line';
+
+describe('drawLine', () => {
+    it('should throw an error for invalid values within the screen array', () => {
+        const screen: number[] = [0, 256, -1, 0];
+        const width = 8;
+        const x1 = 3;
+        const x2 = 7;
+        const y = 2;
+
+        expect(() => {
+            drawLine(screen, width, x1, x2, y);
+        }).toThrow('Invalid screen representation');
+    });
+
+    it('should throw an error if the width is not divisible by 8', () => {
+        const screen: number[] = [0, 0, 0, 0];
+        const width = 13;
+        const x1 = 3;
+        const x2 = 7;
+        const y = 2;
+
+        expect(() => {
+            drawLine(screen, width, x1, x2, y);
+        }).toThrow('Invalid width. It must be divisible by 8');
+    });
+
+    it('should throw an error if y is larger than the height of the screen', () => {
+        const screen: number[] = [0, 0, 0, 0];
+        const width = 8;
+        const x1 = -1;
+        const x2 = 8;
+        const y = 2;
+
+        expect(() => {
+            drawLine(screen, width, x1, x2, y);
+        }).toThrow('Invalid x position. Value must be between 0 and 7');
+    });
+
+    it('should throw an error if x is larger than the width of the screen', () => {
+        const screen: number[] = [0, 0, 0, 0];
+        const width = 8;
+        const x1 = 3;
+        const x2 = 7;
+        const y = 10;
+
+        expect(() => {
+            drawLine(screen, width, x1, x2, y);
+        }).toThrow('Invalid y position. Value must be between 0 and 3');
+    });
+
+    it('should return the 2 dimensional screen array representing the drawn horizontal line with ones if x2 > x1', () => {
+        const screen: number[] = [0, 0, 0, 0];
+        const width = 8;
+        const x1 = 3;
+        const x2 = 7;
+        const y = 2;
+        const expected: number[][] = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+
+        expect(drawLine(screen, width, x1, x2, y)).toEqual(expect.arrayContaining(expected));
+    });
+
+    it('should return the 2 dimensional screen array representing the drawn horizontal line with ones if x1 > x2', () => {
+        const screen: number[] = [0, 0, 0, 0];
+        const width = 8;
+        const x1 = 7;
+        const x2 = 3;
+        const y = 2;
+        const expected: number[][] = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+
+        expect(drawLine(screen, width, x1, x2, y)).toEqual(expect.arrayContaining(expected));
+    });
+
+    it('should return the 2 dimensional screen array representing a dot with a single one if x1 = x2', () => {
+        const screen: number[] = [0, 0, 0, 0];
+        const width = 8;
+        const x1 = 5;
+        const x2 = 5;
+        const y = 2;
+        const expected: number[][] = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+        ];
+
+        expect(drawLine(screen, width, x1, x2, y)).toEqual(expect.arrayContaining(expected));
+    });
+});
---
+++ src/bit-manipulation/draw-line.ts
@@ -0,0 +1,50 @@
+/**
+ * Draws a horizontal line on the screen
+ * @param screen Single array of bytes, allowing 8 consecutive pixels to be stored in one byte
+ * @param width Width of screen in pixels which should be divisible by 8
+ * @param x1 Starting point of the line to be drawn on X axis
+ * @param x2 Ending point of the line to be drawn on X axis
+ * @param y Number of row where the line has to be drawn
+ */
+export function drawLine(screen: number[], width: number, x1: number, x2: number, y: number): number[][] {
+    const maxValue = 255;
+    const minValue = 0;
+    if (screen.some((byte) => byte > maxValue || byte < minValue)) {
+        throw new Error('Invalid screen representation');
+    }
+    if (width % 8 !== 0) {
+        throw new Error('Invalid width. It must be divisible by 8');
+    }
+    const screenBinary: number[] = screen.reduce((previous, current) => {
+        let binary = current.toString(2);
+        const diff = 8 - binary.length;
+        binary = '0'.repeat(diff).concat(binary);
+        const binaryArr = binary
+            .split('')
+            .map((bit) => parseInt(bit, 2));
+        previous.push(...binaryArr);
+        return previous;
+    }, new Array<number>());
+    const screenXY: number[][] = screenBinary.reduce((previous, current, index) => {
+        const rowIndex = Math.floor(index / width);
+        if (index % width === 0) {
+            previous.push([]);
+        }
+        previous[rowIndex].push(current);
+        return previous;
+    }, new Array<number[]>());
+    if (y < 0 || y > screenXY.length - 1) {
+        throw new Error(`Invalid y position. Value must be between 0 and ${screenXY.length - 1}`);
+    }
+    const screenRow = screenXY[y];
+    if (x1 < 0 || x2 < 0 || x1 > screenRow.length - 1 || x2 > screenRow.length - 1) {
+        throw new Error(`Invalid x position. Value must be between 0 and ${screenRow.length - 1}`);
+    }
+    const startX = x1 < x2 ? x1 : x2;
+    const endX = x2 > x1 ? x2 : x1;
+
+    for (let i = startX; i <= endX; i++) {
+        screenRow[i] = 1;
+    }
+    return screenXY;
+}
