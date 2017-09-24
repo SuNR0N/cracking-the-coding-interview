60d2b35a982ec7d06378965edf00c050ef2a01c8
Norbert Annus <norberta@Norberts-MacBook-Pro.local>
2017-09-24 19:30:00 +0100

    Implement Permutations without Dups

--- src/recursion-and-dynamic-programming/permutations-without-dups.ts
+++ src/recursion-and-dynamic-programming/permutations-without-dups.ts
@@ -1,4 +1,27 @@
 /** Computes all permutations of a string of unique characters */
 export function permutationsWithoutDups(input: string): string[][] {
-    throw new Error('Not implemented');
+    const chars = input.split('');
+    chars.reduce((previous, current) => {
+        const count = previous.get(current);
+        if (count) {
+            throw new Error('Invalid input');
+        } else {
+            previous.set(current, 1);
+        }
+        return previous;
+    }, new Map<string, number>());
+    let permutations: string[][] = [];
+    for (let len = chars.length; len > 0; len--) {
+        if (permutations.length === 0) {
+            permutations.push(...chars.map((char) => [char]));
+        } else {
+            permutations = permutations.reduce((previous, current) => {
+                chars.filter((char) => !current.includes(char))
+                    .map((char) => current.slice().concat(char))
+                    .forEach((perm) => previous.push(perm));
+                return previous;
+            }, new Array<string[]>());
+        }
+    }
+    return permutations;
 }
